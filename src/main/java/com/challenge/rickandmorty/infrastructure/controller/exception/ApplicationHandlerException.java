616e29c162013653fe6641413e90e866e259b4e6
Rodrigo <rodribot01@icloud.com>
2022-11-14 15:37:12 -0300

    Mejoras en las exceptions

--- src/main/java/com/challenge/rickandmorty/infrastructure/controller/exception/ApplicationHandlerException.java
+++ src/main/java/com/challenge/rickandmorty/infrastructure/controller/exception/ApplicationHandlerException.java
@@ -5,35 +5,27 @@ import com.challenge.rickandmorty.application.exception.HttpServerRickAndMortyEx
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
-import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
 public class ApplicationHandlerException {
-    @ExceptionHandler(value = CharacterNotFoundException.class)
-    public ResponseEntity<Map<String,String>> handlerUserNotFoundException(){
-
-        Map<String,String> map = new HashMap<>();
-        map.put("message","Character not found");
-        return new ResponseEntity<>(map, HttpStatus.NOT_FOUND);
+    @ExceptionHandler(CharacterNotFoundException.class)
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    public Map<String, String> handlerUserNotFoundException(){
+        return Map.of("message","Character not found");
     }
 
-    @ExceptionHandler(value = HttpServerRickAndMortyException.class)
+    @ExceptionHandler(HttpServerRickAndMortyException.class)
     public ResponseEntity<Map<String,String>> handlerServerErrorException(){
-
-        Map<String,String> map = new HashMap<>();
-        map.put("error","Server error");
-        return new ResponseEntity<>(map, HttpStatus.INTERNAL_SERVER_ERROR);
+        return ResponseEntity.internalServerError().body(Map.of("error","Server error"));
     }
 
-    @ExceptionHandler(value = MethodArgumentTypeMismatchException.class)
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     public ResponseEntity<Map<String,String>> MethodArgumentTypeMismatch(){
-
-        Map<String,String> map = new HashMap<>();
-        map.put("error","Hey! you must provide an id");
-        return new ResponseEntity<>(map, HttpStatus.BAD_REQUEST);
+        return ResponseEntity.badRequest().body(Map.of("error","Hey! you must provide an id"));
     }
 }
